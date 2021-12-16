fa9055ea6c8c9e7c5f577966b898be1f91272d3c
Joaquin Ribero <joaquin.ribero@mercadolibre.com>
2021-12-16 17:00:02 -0300

    Ejercicio5

---
+++ GO Bases TT/Ejercicio5/main.go
@@ -0,0 +1,30 @@
+package main
+
+import ("fmt")
+
+func main() {
+
+	var s []string 
+	s = append(s,"Benjamin", 
+		"Nahuel", 
+		"Brenda", 
+		"Marcos", 
+		"Pedro", 
+		"Axel", 
+		"Alez", 
+		"Dolores", 
+		"Federico", 
+		"Hernán", 
+		"Leandro", 
+		"Eduardo", 
+		"Duvraschka")
+	// Estudiantes originales
+	fmt.Println(s)
+
+	if dias:=2; dias>=2 {
+		s = append(s, "Gabriela")
+	}
+	fmt.Println("Pasados 2 clases:")
+	fmt.Println(s)
+	
+}
