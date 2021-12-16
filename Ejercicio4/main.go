c9d746e739295fdd2f970f17c48a294beff820df
Joaquin Ribero <joaquin.ribero@mercadolibre.com>
2021-12-16 11:59:35 -0300

    Ejercicio 4

---
+++ Ejercicio4/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	
+	var apellido string = "Gomez"
+
+  	// var edad int = "35" incorrecto
+	var edad int = 35 //correcto
+
+  	//boolean := "false"; incorrecto
+	boolean := false //correcto
+
+  	// var sueldo string = 45857.90 incorrecto
+	var sueldo float = 45857.90 //correcto
+	
+  	var nombre string = "Julián"
+
+}
