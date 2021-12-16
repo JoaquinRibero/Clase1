1c0d01ba320212bbe1a3ad45088090f46035e560
Joaquin Ribero <joaquin.ribero@mercadolibre.com>
2021-12-16 16:14:39 -0300

    Imprimir todas las letras de la palabra (ejercicio 1)

--- GO Bases TT/Ejercicio1/main.go
+++ GO Bases TT/Ejercicio1/main.go
@@ -9,4 +9,8 @@ func main() {
 	n := len(palabra);
 
 	fmt.Printf("La cantidad de letras de la palabra \"%s\" es: %d\n", palabra, n)
+
+	for _, value := range palabra {
+		fmt.Printf("%c \n", value)
+	}
 }
