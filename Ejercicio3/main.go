e7740a8e75a76240c87d121c3c572bf277cf35b7
Joaquin Ribero <joaquin.ribero@mercadolibre.com>
2021-12-16 11:48:18 -0300

    Ejercicio 3

---
+++ Ejercicio3/main.go
@@ -0,0 +1,7 @@
+// var 1nombre string  incorrecta
+var apellido string
+var int edad
+// 1apellido := 6 incorrecta
+var licencia_de_conducir = true
+// var estatura de la persona int incorrecta
+cantidadDeHijos := 2
