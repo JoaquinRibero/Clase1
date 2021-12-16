4f340e3670952559fa5bcf42732d5c86c71162aa
Joaquin Ribero <joaquin.ribero@mercadolibre.com>
2021-12-16 16:02:25 -0300

    Ejercicio1 Bases TT

---
+++ GO Bases TT/Ejercicio1/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var palabra string = "academia"
+
+	n := len(palabra);
+
+	fmt.Printf("La cantidad de letras de la palabra \"%s\" es: %d\n", palabra, n)
+}
