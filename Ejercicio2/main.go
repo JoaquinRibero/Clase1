73509ab3c47b34d81b32ae0f972a7d6f1c8e6b66
Joaquin Ribero <joaquin.ribero@mercadolibre.com>
2021-12-16 11:44:59 -0300

    Ejercicio2

---
+++ Ejercicio2/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	
+	var temperatura int = 29
+	var presion int = 1014
+	var humedad uint = 51
+
+	fmt.Printf("La temperatura es: %d \n" ,temperatura)
+	fmt.Printf("La presiòn es: %d \n" ,presion)
+	fmt.Printf("La humedad es: %d \n", humedad)
+
+}
