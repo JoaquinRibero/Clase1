ce6d6e4df4f48323cf2aaed9dd36e7ce7a8305d8
Joaquin Ribero <joaquin.ribero@mercadolibre.com>
2021-12-16 11:50:40 -0300

    Correccion ejercicio 3

--- Ejercicio3/main.go
+++ Ejercicio3/main.go
@@ -1,7 +1,10 @@
 // var 1nombre string  incorrecta
+var nombre string
 var apellido string
 var int edad
 // 1apellido := 6 incorrecta
+primerApellido := 6
 var licencia_de_conducir = true
 // var estatura de la persona int incorrecta
+var estatura_de_la_persona int
 cantidadDeHijos := 2
